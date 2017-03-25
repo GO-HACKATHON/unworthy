698cd5d3ac243fd1c19207a5fb190c20e2841642
gaferage <fransisco.antoni.gea@gmail.com>
2017-03-25 15:43:29 +0700

    UpdateMap

--- android/Unworthy/app/src/main/java/gohackathon/unworthy/MapsActivity.java
+++ android/Unworthy/app/src/main/java/gohackathon/unworthy/MapsActivity.java
@@ -3,15 +3,24 @@ package gohackathon.unworthy;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.location.places.PlaceReport;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionApi;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,12 +29,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.*;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnConnectionFailedListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
     private GoogleMap mMap;
-    private  GoogleApiClient mGoogleApiClient;
+    private GoogleApiClient mGoogleApiClient;
+    private  PlaceAutocompleteFragment auFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +46,82 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        auFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        auFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                mMap.clear();
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+                mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getAddress().toString()));
+                Location currLocation = getCurrentLocation();
+                LatLng newLatLng = new LatLng(currLocation.getLatitude(),currLocation.getLongitude());
+                mMap.addPolyline(new PolylineOptions().add(newLatLng,place.getLatLng()).color(Color.RED));
+            }
+
+            @Override
+            public void onError(Status status) {
+
+            }
+        });
     }
 
     private void initWindow() {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION,},123);
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},123);
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 123);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 123);
             return;
         }
         mMap.setMyLocationEnabled(true);
+    }
+
+    private void initGoogleApi(GoogleMap googleMap) {
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addApi(LocationServices.API)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
+
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(15.f));
+
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                setCurrentLocation();
+                return true;
+            }
+        });
+        mMap.getUiSettings().setCompassEnabled(false);
+    }
+
+    private Location getCurrentLocation(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 123);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 123);
+
+        }
+        Location currLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        return currLocation;
+    }
+
+    private void setCurrentLocation(){
+        Location currLocation = getCurrentLocation();
+        // Add a marker in Sydney and move the camera
+        if(currLocation!=null){
+            Log.v("StringDebug","containLocation");
+            LatLng newLocation = new LatLng(currLocation.getLatitude(), currLocation.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(newLocation));
+        }
     }
 
     /**
@@ -60,18 +135,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        
+        initGoogleApi(googleMap);
         initWindow();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Log.v("StringDebug","ConnectionFailed");
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.v("StringDebug","connectedLocation");
+        setCurrentLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.v("StringDebug","ConnectionSuspended");
+    }
+
+
 }
