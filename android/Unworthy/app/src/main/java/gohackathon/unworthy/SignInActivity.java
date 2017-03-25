214e688cb0922111dd0f88fdea2b819e35346fe4
gaferage <fransisco.antoni.gea@gmail.com>
2017-03-25 12:18:25 +0700

    Update Map Activity

--- android/Unworthy/app/src/main/java/gohackathon/unworthy/SignInActivity.java
+++ android/Unworthy/app/src/main/java/gohackathon/unworthy/SignInActivity.java
@@ -1,5 +1,6 @@
 package gohackathon.unworthy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        startActivity(new Intent(this,MapsActivity.class));
     }
 }
