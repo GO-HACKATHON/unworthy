a33887be1b1cce5fa603685224c12093d372b133
Andi Muqsith Ashari <Andi Muqsith Ashari>
2017-03-26 09:17:37 +0700

    Add pass list for every vector

--- backend-php/get-map.php
+++ backend-php/get-map.php
@@ -12,6 +12,12 @@ include "helpers/lineCircle.php";
 $eventList = file_get_contents("https://mars.aashari.id/api/get-events.json");
 $eventList = json_decode($eventList);
 
+$driverPassList = [];
+$driverPassData = $mysqli->query("SELECT * FROM tracker ORDER BY location_x");
+while ($driverPass = $driverPassData->fetch_assoc()) {
+    $driverPassList[] = $driverPass;
+}
+
 $response = [];
 
 $graphList = $mysqli->query("SELECT * FROM map_point ORDER BY location_x");
@@ -20,6 +26,12 @@ while ($graph = $graphList->fetch_assoc()) {
     $relationList = $mysqli->query("SELECT mp2.*,mv.type as type FROM map_vector mv JOIN map_point mp1 ON mp1.id=mv.point_1 JOIN map_point mp2 ON mp2.id=mv.point_2 WHERE mp1.id = '" . $graph['id'] . "'");
     while ($relation = $relationList->fetch_assoc()) {
         $pathEvent = [];
+        $passList = [];
+        foreach ($driverPassList as $dp) {
+            if (lineCircle($graph['location_x'], $graph['location_y'], $relation['location_x'], $relation['location_y'], $dp['location_x'], $dp['location_y'])) {
+                $passList[] = $dp;
+            }
+        }
         foreach ($eventList as $event) {
             $el = $event->location;
             if (is_array($el)) {
@@ -39,6 +51,7 @@ while ($graph = $graphList->fetch_assoc()) {
         }
         $relation['distance'] = distance($graph['location_x'], $graph['location_y'], $relation['location_x'], $relation['location_y']);
         $relation['events'] = $pathEvent;
+        $relation['pass_list'] = $passList;
         $graph['relation'][] = $relation;
     }
     $response[] = $graph;
